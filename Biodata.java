e119020357b83463fb3edfae16d716f97df7f1aa
VINCENTSAMUVEL <samuvelvvincent@gmail.com>
2024-05-07 00:40:11 +0530

    Biodata.java

---
+++ Biodata.java
@@ -0,0 +1,30 @@
+public class Biodata{
+	
+	public static void main(String[] agrs){
+
+	System.out.println("Bio Data");
+
+    name();
+    degree();
+    percentage();
+
+	}
+
+	public static void name(){
+
+	System.out.println("Vincent Samuvel");
+
+	}
+
+	public static void degree(){
+
+	System.out.println("Bachelor Of Computer Application");
+
+	}
+
+	public static void percentage(){
+
+	System.out.println("Seventy two");
+
+	}
+}
