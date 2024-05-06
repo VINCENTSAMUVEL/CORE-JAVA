28902c039c4561f17608974e459592e9bb2bf105
VINCENTSAMUVEL <samuvelvvincent@gmail.com>
2024-05-07 00:57:17 +0530

    Methods.java

---
+++ Methods.java
@@ -0,0 +1,30 @@
+public class Methods{
+	
+	public static void main(String[] args){
+
+	System.out.println("Methods In Java");
+    
+    publi();
+    statis();
+    string();
+
+	}
+
+	public static void publi(){
+
+	System.out.println("Access Specifier");
+
+	} 
+
+	public static void statis(){
+
+	System.out.println("Non Access Modifier");
+
+	}
+
+	public static void string(){
+
+	System.out.println("parameters");
+
+	}
+}
