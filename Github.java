9488b07de177d1cbad799165fa3dd37aacb2df3e
VINCENTSAMUVEL <samuvelvvincent@gmail.com>
2024-05-07 00:10:52 +0530

    Github.java

---
+++ Github.java
@@ -0,0 +1,26 @@
+public class Github{
+	
+	public static void main(String[] args){
+
+	System.out.println("Github");
+
+     gitAdd();
+     gitCommit();
+     gitPush();
+
+	}
+
+	public static void gitAdd(){
+
+		System.out.println("Github.java");
+	}
+
+	public static void gitCommit(){
+		System.out.println("Added text file");
+	}
+
+    public static void gitPush(){
+
+    	System.out.println("Origin master");
+    }
+}
