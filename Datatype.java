03f0069e5ed0c1517773749f4be199d24ef2234a
VINCENTSAMUVEL <samuvelvvincent@gmail.com>
2024-05-06 21:35:42 +0530

    Datatype.java

--- Datatype.java
+++ Datatype.java
@@ -3,5 +3,16 @@ public class Datatype{
 	public static void main(String[] args){
 
 		System.out.println("Types of datas");
+
+datas();
+
+	}
+
+
+
+	public static void datas(){
+
+		System.out.println("Primitive Datatype");
 	}
 }
+
