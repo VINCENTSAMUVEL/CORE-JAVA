abb9047cc954c45d7f9f93bf00811710ea799d3a
VINCENTSAMUVEL <samuvelvvincent@gmail.com>
2024-05-07 00:28:06 +0530

    Seasons.java

---
+++ Seasons.java
@@ -0,0 +1,27 @@
+public class Seasons{
+
+	public static void main(String[] args){
+
+	System.out.println("Seasons");
+
+    summer();
+    rainny();
+    winter();
+	}
+
+	public static void summer(){
+
+	System.out.println("Hot");
+	}
+
+    public static void rainny(){
+
+    System.out.println("Rain");
+
+    }
+    public static void winter(){
+
+    System.out.println("cold");
+
+    }
+}
